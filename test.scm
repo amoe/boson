4006adebac17f0b157c74e10fa5f976aefbb84c5
David Banks <amoebae@gmail.com>
2011-04-21 16:34:18 +0100

    tests for compat and session-util modules
    Ignore-this: 86c9ced6a2c36fe54373129e53d04cca
    
    darcs-hash:20110421153418-ebcb5-9b5d91710a1f86cd354723aaa60444100e330691.gz

--- test.scm
+++ test.scm
@@ -1,6 +1,8 @@
 (import (rnrs)
         (prefix (boson globals) globals:)
         (prefix (boson http) http:)
+        (prefix (boson compat) compat:)
+        (prefix (boson session-util) session-util:)
         (sistim wrap64))
 
 (test-begin "globals")
@@ -8,8 +10,42 @@
 (test-end "globals")
 
 ; Make sure (boson http) exports all the bindings shown in the Spark manual.
+; We don't test these functions here as the http library is just re-exporting
+; from other modules.
 (test-begin "http")
 (test-assert http:web-server)
 (test-assert http:web-server-start)
 (test-assert http:web-server-stop)
 (test-end "http")
+
+(test-begin "compat")
+(test-equal "txt" (compat:filename-extension "readme.txt"))
+(test-equal "gz" (compat:filename-extension "package.tar.gz"))
+(test-assert (not (compat:filename-extension "README")))
+
+; We don't test the mosh-specific functions as they will go away.
+(test-end "compat")
+
+(test-begin "session-util")
+(let ((state (session-util:make-default-session-state 'nonesuch))
+      (key "page") (value "index"))
+  (test-assert (hashtable? state))   ; this type is specified, implementation is
+                                     ; not abstract.  user can use hash procedures.
+  (session-util:http-value! state key value)
+  (test-equal value (session-util:http-value state key "default"))
+  (test-assert (not (session-util:http-value state "not-present")))
+  (test-assert (not (session-util:http-keep-alive? state)))
+  (session-util:http-keep-alive! state #t)
+  (test-assert (session-util:http-keep-alive? state))
+  (test-assert (session-util:http-share-state? state))
+  (session-util:http-share-state! state #f)
+  (test-assert (not (session-util:http-share-state? state))))
+  
+
+; http-call is used to throw procedures around with exceptions.
+(let ((proc (lambda (x) x)))   ; never actually called, just type-checked
+  (test-error procedure? (session-util:http-call proc))
+  (test-error string?    (session-util:http-call 1234)))   ; error case.
+  
+;; (test-assert session-util:make-default-session-state)
+(test-end "session-util")
